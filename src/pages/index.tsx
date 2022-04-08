6c010615e879f01ae2e2ca62bd2b75e4173b46a8
kim hye ri <hrkim2@seegene.com>
2022-04-08 13:37:38 +0900

    getStaticProps 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
 import type { NextPage } from 'next';
 import HomeComponent from 'component/Templates/Home';
+import {getAllSortedPost} from 'data/blog';
 
-const Home: NextPage = ({post}:any) => {  
+const Home: NextPage = ({post}:any) => {
   return <HomeComponent blogMetaList={post}/>
 }
 
 export default Home;
 
-
 export async function getStaticProps(){
-  const response = await fetch('http://localhost:3000/api/getPosts');
-  const post = await response.json();
+  const post = await getAllSortedPost();
 
   return {
     props: {
