0c3f4f7457700b8fc9987603b6fd5f2c4e943257
kim hye ri <hrkim2@seegene.com>
2022-04-08 14:03:01 +0900

    base path 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: process.env.NODE_ENV === "production" ? "/action-text" : "/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
